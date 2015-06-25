c7151a9f89dc5e4857648f8d55f84213c900a8c6
Jonathan Hartman <j@p4nt5.com>
2015-06-24 22:14:44 -0700

    Initial commit--a freshly generated cookbook

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+Gimp Cookbook CHANGELOG
+=======================
+
+v0.0.1 (2015-06-24)
+-------------------
+- Development started
---
+++ metadata.rb
@@ -0,0 +1,11 @@
+# Encoding: UTF-8
+#
+# rubocop:disable SingleSpaceBeforeFirstArg
+name             'gimp'
+maintainer       'Jonathan Hartman'
+maintainer_email 'j@p4nt5.com'
+license          'apache2'
+description      'Installs/Configures gimp'
+long_description 'Installs/Configures gimp'
+version          '0.0.1'
+# rubocop:enable SingleSpaceBeforeFirstArg
